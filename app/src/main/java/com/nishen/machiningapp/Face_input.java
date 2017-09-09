11d55d49fdd4880c70f84a592fc4da1bf3e54c67
Nishen <nramsugit@gmail.com>
2017-09-09 21:49:51 +0200

    Added cut profile page.

---
+++ app/src/main/java/com/nishen/machiningapp/Face_input.java
@@ -0,0 +1,13 @@
+package com.nishen.machiningapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Face_input extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_input);
+    }
+}
